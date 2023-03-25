453c9fcee73de172020b463fe6b2b5a00556ef01
moff <moffmuriithi98@gmail.com>
2023-03-25 14:43:56 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,35 @@
+// Basic math functions
+function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  function increment(n) {
+    return ++n;
+  }
+  
+  function decrement(n) {
+    return --n;
+  }
+  
+  // Parse integer from string
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  
+  // Preserve decimal from string
+  function preserveDecimal(n) {
+    return parseFloat(n,10);
+  }
+  
